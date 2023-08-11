ae377f3b6f8e938720f1001f8953be73e3c304f3
Maggie <82394207+MaggieHDez@users.noreply.github.com>
2023-08-11 00:08:28 -0600

    Update app.js

--- Proj3 - WeatherJournal/website/app.js
+++ Proj3 - WeatherJournal/website/app.js
@@ -1,5 +1,28 @@
 /* Global Variables */
+const API_KEY = '0a14f53bb278bb798efca511e2fbb13d&units=imperial'; // Personal API Key for OpenWeatherMap API
+const baseUrl = 'https://api.openweathermap.org/data/2.5/weather';
 
 // Create a new date instance dynamically with JS
 let d = new Date();
 let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+
+const postData = async ( url = '', data = {})=>{
+    console.log(data);
+      const response = await fetch(url, {
+      method: 'POST',
+      credentials: 'same-origin',
+      headers: {
+          'Content-Type': 'application/json',
+      },
+     // Body data type must match "Content-Type" header
+      body: JSON.stringify(data),
+    });
+
+      try {
+        const newData = await response.json();
+        console.log(newData);
+        return newData;
+      }catch(error) {
+      console.log("error", error);
+      }
+  }
