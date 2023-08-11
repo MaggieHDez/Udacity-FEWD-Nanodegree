cd62ac986c33f45a930bddf148b2b5fdf9a4012b
Maggie <82394207+MaggieHDez@users.noreply.github.com>
2023-08-11 00:07:25 -0600

    Adding modules

--- Proj3 - WeatherJournal/server.js
+++ Proj3 - WeatherJournal/server.js
@@ -1,19 +1,41 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+ let projectData = {};
 
 // Require Express to run server and routes
+const express = require('express');
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const port = 8080;
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
+
+// Routes
+
+// GET
+app.get('/all', function (req, res) {
+    res.send(projectData);
+  });
+
+// POST
+app.post("/generateWeatherData", weatherData);
+
+function weatherData(req, res) {
+    data = req.body;
+    res.send(data)
+}
